c70dec2e7e3248ef6537b84c89e9b9e66a2459aa
avabellaw <70448139+avabellaw@users.noreply.github.com>
2025-01-17 13:42:46 +0000

    feat: Add toggleable boolean to show story on homepage

--- stories/admin.py
+++ stories/admin.py
@@ -10,3 +10,12 @@ class StoryAdmin(admin.ModelAdmin):
     '''
 
     list_display = ('title', 'content', 'date_created')
+    actions = ['add_to_homepage', 'remove_from_homepage']
+
+    def add_to_homepage(self, queryset):
+        '''Action to add selected story to the homepage stories section'''
+        queryset.update(show_on_homepage=True)
+
+    def remove_from_homepage(self, queryset):
+        '''Action to remove selected story from the homepage stories section'''
+        queryset.update(show_on_homepage=False)
--- stories/models.py
+++ stories/models.py
@@ -7,6 +7,7 @@ class Story(models.Model):
     title = models.CharField(max_length=15, unique=True)
     content = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
+    show_on_homepage = models.BooleanField(default=False)
 
     class Meta:
         '''Meta options for the story model'''
