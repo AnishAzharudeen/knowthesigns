ace694d1e27d6cecc1593f69d3fce1dadcb67ec9
avabellaw <70448139+avabellaw@users.noreply.github.com>
2025-01-17 13:22:30 +0000

    Create model for a story

--- stories/models.py
+++ stories/models.py
@@ -1,3 +1,13 @@
+'''Models for the stories app'''
 from django.db import models
 
-# Create your models here.
+
+class Story(models.Model):
+    '''Model for a story'''
+    title = models.CharField(max_length=15, unique=True)
+    content = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        '''String representation of a story'''
+        return str(self.title)
