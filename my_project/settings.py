592d2cd339ea506182a12e2b48b342f6b5116eda
Anish Fatima <fatima.anish@gmail.com>
2025-01-18 11:41:02 +0000

    Set debug value to false and added heroku to allowed host

--- my_project/settings.py
+++ my_project/settings.py
@@ -34,9 +34,9 @@ SECRET_KEY = (
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
 
 
 # Application definition
