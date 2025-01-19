2c4d5bda50b0cf3154eaa715c59976cf109a808b
Sebastian <sebastian.kefer@gmail.com>
2025-01-19 14:44:11 +0100

    homepage and styling

--- home/views.py
+++ home/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from reports.forms import ReportForm
+from stories.models import Story
 
 
 def home(request):
     """Home page view"""
-    context = {"report_form": ReportForm}
+    homepage_stories = Story.objects.filter(show_on_homepage=True)
+    context = {"report_form": ReportForm, "homepage_stories": homepage_stories}
     return render(request, "home/index.html", context)
