4d7c04a37906e8a21e2ace544bf9476926c6638f
avabellaw <avabellawilliams@gmail.com>
2025-01-19 13:25:01 +0000

    Create advice app and move over template

--- stories/views.py
+++ stories/views.py
@@ -6,11 +6,3 @@ def stories(request):
     """Main page to show victim stories"""
     context = {"stories": Story.objects.all().order_by("-show_on_homepage")}
     return render(request, "stories/stories.html", context)
-
-
-def advice(request):
-    """
-    A view to show the advice page
-    """
-
-    return render(request, "advice/advice.html")
